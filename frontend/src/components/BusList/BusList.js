521cc15ced8e4d8a8c919de40d1332d00beeb604
subrahmanyammathi <sivasubrahmanyammathi@gmail.com>
2025-11-04 14:57:29 +0530

    Fix 3

--- frontend/src/components/BusList/BusList.js
+++ frontend/src/components/BusList/BusList.js
@@ -3,15 +3,10 @@ import { FaAngleDoubleDown } from "react-icons/fa";
 import './busList.css'
 export default function BusList({ value: dataInp }) {
 
-    const [obj, setObj] = useState('')
     const [reset, Setreset] = useState(false)
     const [arrowDown, setArrowDown] = useState(false)
     const [clas, SetClas] = useState(true)
 
-
-    useEffect(() => {
-        setObj(dataInp)
-    }, [dataInp])
 
 
     const handleSubmit = bId => {
@@ -45,14 +40,14 @@ export default function BusList({ value: dataInp }) {
                         <div class="w-100 d-none d-md-block"></div>
 
                         {console.log(bus.seatArray)}
-                        <div class="col-6 col-sm-3 mb-4">{bus.companyName}</div>
-                        <div class="col-6 col-sm-3 mb-4">{bus.startCity}</div>
-                        <div class="col-6 col-sm-3 mb-4">{bus.destination}</div>
-                        <div class="col-6 col-sm-3 mb-4">{bus.pricePerSeat}</div>
-                        <div class="col-6 col-sm-4 mb-2 ml-0">
-                            <button className={clas ? "btn btn-primary btn-md" : "btn btn-primary btn-md disabled"} onClick={(bId) => { handleSubmit(bus._id) }} >Book Now</button>
+                        <div className="col-6 col-sm-3 mb-4">{bus.companyName}</div>
+                        <div className="col-6 col-sm-3 mb-4">{bus.startCity}</div>
+                        <div className="col-6 col-sm-3 mb-4">{bus.destination}</div>
+                        <div className="col-6 col-sm-3 mb-4">{bus.pricePerSeat}</div>
+                        <div className="col-6 col-sm-4 mb-2 ml-0">
+                            <button className={clas ? "btn btn-primary btn-md" : "btn btn-primary btn-md disabled"} onClick={() => handleSubmit(bus._id)} >Book Now</button>
                         </div>
-                        <div class="col-6 col-sm-4 mb-2 ml-0">
+                        <div className="col-6 col-sm-4 mb-2 ml-0">
                             <span className={reset ? "badge badge-danger ml-5" : "disabled"} onClick={e => handleReset(e)}>Reset</span>
                         </div>
                     </div>
--- frontend/src/components/PaymentTab/PaymentTab.js
+++ frontend/src/components/PaymentTab/PaymentTab.js
@@ -7,7 +7,7 @@ import {
     formatCreditCardNumber,
     formatCVC,
     formatExpirationDate,
-    formatFormData
+    
 } from './utils'
 import 'react-credit-cards/es/styles-compiled.css'
 
@@ -57,7 +57,6 @@ export default class App extends React.Component {
 
     handleSubmit = e => {
         e.preventDefault()
-        const { issuer } = this.state
         const formData = [...e.target.elements]
             .filter(d => d.name)
             .reduce((acc, d) => {
@@ -122,9 +121,7 @@ export default class App extends React.Component {
             expiry,
             cvc,
             focused,
-            issuer,
-            formData,
-            token
+            issuer
         } = this.state
 
         return ( <
